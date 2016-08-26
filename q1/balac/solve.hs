8824cc60d80831a2c0b05dc4173afb1f95258be0
Balachandran Chandrasekharan <balachandran.c@gmail.com>
2016-08-27 00:26:00 +0530

    q1: scary words

---
+++ q1/balac/solve.hs
@@ -0,0 +1,14 @@
+import System.IO
+import Data.Char
+
+intVal :: Char -> Int
+intVal c = ord c - 96
+
+isScary :: [Char] -> Bool
+isScary word = ( sum $ map intVal word ) == 26
+
+main = do
+    contents <- readFile "words.txt"
+    let words = lines $ map toLower contents
+    let scaryWords = filter isScary words
+    putStr $ show scaryWords
