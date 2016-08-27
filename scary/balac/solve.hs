6656b539f8d05aa56e2c795add9345001150becb
Balachandran Chandrasekharan <balachandran.c@gmail.com>
2016-08-27 18:49:08 +0530

    scary: Fixes based on @neongreen 's suggestions.

--- scary/balac/solve.hs
+++ scary/balac/solve.hs
@@ -2,13 +2,13 @@ import System.IO
 import Data.Char
 
 intVal :: Char -> Int
-intVal c = ord c - 96
+intVal c = ord ( toLower c ) - 96
 
 isScary :: [Char] -> Bool
-isScary word = ( sum $ map intVal word ) == 13
+isScary word = ( sum $ map intVal $ filter isAsciiLower word ) == 13
 
 main = do
     contents <- readFile "words.txt"
-    let words = lines $ map toLower contents
+    let words = lines contents
     let scaryWords = filter isScary words
-    putStr $ show scaryWords
+    print scaryWords
