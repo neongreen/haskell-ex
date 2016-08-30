3994de716f968eec35aaa44cafd0a3d2a323f7fa
Balachandran Chandrasekharan <balachandran.c@gmail.com>
2016-08-30 12:06:20 +0530

    reposts: feedback from @neongreen and @jonaprieto.

--- reposts/balac/solve.hs
+++ reposts/balac/solve.hs
@@ -8,30 +8,26 @@ kNumWinners = 7 :: Int
 kNumTrials = 10000 :: Int
 
 getNonMember :: [Int] -> Int -> IO Int
-getNonMember lst range = do
-    n <- getStdRandom( randomR( 1, range ) )
-    if n `elem` lst
-        then getNonMember lst range
-        else return n
+getNonMember lst range = head . filter ( `notElem` lst ) . randomRs ( 1, range ) <$> newStdGen
 
 pickNums :: Int -> Int -> IO [Int]
 pickNums 0 _ = return []
 pickNums count range = do
     rest <- pickNums ( count - 1 ) range
     newVal <- getNonMember rest range
-    return ( newVal : rest )
+    return $ newVal : rest
 
 isWin :: Int -> Int -> Int -> IO Bool
 isWin numExistingPosts numPosts numWinners =  do
     let totalPosts = numExistingPosts + numPosts
     drawnNums <- pickNums numPosts totalPosts
-    return $ any id $ map ( <= numWinners ) drawnNums
+    return $ any ( <= numWinners ) drawnNums
 
 winProb :: Int -> Int -> Int -> IO Float
 winProb numExistingPosts numPosts numTrials = do
-    results <- sequence $ take numTrials $ repeat ( isWin numExistingPosts numPosts kNumWinners )
+    results <- replicateM numTrials $ isWin numExistingPosts numPosts kNumWinners
     return $ fromIntegral( length ( filter id results ) ) / ( fromIntegral numTrials )
 
 main = do
-    results <- sequence $ map (\x -> winProb kNumExistingPosts x kNumTrials ) kNumPosts
+    results <- mapM (\x -> winProb kNumExistingPosts x kNumTrials ) kNumPosts
     print results
