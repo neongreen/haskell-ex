2f7f0941da233526d53702ec1d3cf9225884511e
Jason Kuhrt <jasonkuhrt@me.com>
2016-08-31 20:04:18 -0400

    scary: letter-to-number function

---
+++ scary/jasonkuhrt/Main.hs
@@ -0,0 +1,29 @@
+-- # Scary
+
+-- Given a letter-to-number mapping, find all "scary" words in the dictionary.
+-- The mapping system is A=1, B=2, ..., Z=26.
+-- A "scary" word is any word whose sum is 13.
+-- The dictionary can be found at /usr/share/dict/words (macOS)
+
+import qualified Data.Char as Char
+
+
+
+main :: IO ()
+main = undefined
+
+letterValue :: Char -> Integer
+letterValue =
+  doLetterValue .
+  toInteger .
+  Char.ord .
+  Char.toLower
+  where
+  doLetterValue :: Integer -> Integer
+  doLetterValue charOrd
+    | charOrd >= 65 && charOrd <= 122 =
+      toInteger $ charOrd - offset
+    | otherwise =
+      0
+    where
+    offset = 96
