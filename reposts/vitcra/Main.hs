0c308f7e492109f6dad0299d80afc84bab96f699
Vitalie Cracan <vitalie.cracan@gmail.com>
2016-09-02 09:31:48 +0300

    do not split the generator, use the one returned

--- reposts/vitcra/Main.hs
+++ reposts/vitcra/Main.hs
@@ -8,25 +8,23 @@ import qualified Data.Set as Set
 data Experiment = Experiment {my::Int, other::Int, prizes::Int}
 
 -- returns 0 if we did not win, 1 if we did
-draw :: RandomGen g => g -> Experiment -> Int
-draw g Experiment{..} = min 1 . hits $ rs where
+draw :: RandomGen g => g -> Experiment -> (Int, g)
+draw g Experiment{..} = (min 1 . hits . fst $ rs, snd rs) where
   hits = Set.size . Set.filter (<= my)
   rs = helper g prizes Set.empty
-  helper g 0 xs = xs
+  helper g 0 xs = (xs, g)
   helper g n xs =
     if Set.member r xs
-    then helper g'' n xs
-    else helper g'' (n-1) (Set.insert r xs)
+    then helper g' n xs
+    else helper g' (n-1) (Set.insert r xs)
     where
-      (g', g'') = split g
-      r = fst $ randomR (1, my + other) g'
+      (r, g') = randomR (1, my + other) g
 
 repeatDraw :: RandomGen g => g -> Int -> Experiment -> [Int]
 repeatDraw g n e = helper g n [] where
   helper g 0 xs = xs
-  helper g n xs = helper g'' (n-1) (d:xs) where
-    (g', g'') = split g
-    d = draw g' e
+  helper g n xs = helper g' (n-1) (d:xs) where
+    (d, g') = draw g e
 
 
 otherReposts = 1000000
