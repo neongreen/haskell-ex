f8e998d6ceb28c05c6f8a57f298dabd5552443b3
Vitalie Cracan <vitalie.cracan@gmail.com>
2016-09-03 09:21:29 +0300

    logic-brute: with feedback from Artyom

--- logic-brute/vitcra/Main.hs
+++ logic-brute/vitcra/Main.hs
@@ -4,23 +4,11 @@ import           Data.List
 
 type Pair = (Int, Int)
 
-udiv :: Int -> Int -> Int
-udiv n m
-  | rem n m == 0 = div n m
-  | otherwise = 1 + div n m
-
-divisors :: Int -> [Int]
-divisors n = filter ((== 0) . rem n) [2 .. div n 2]
-
 factorPairs :: Int -> [Pair]
-factorPairs n = map pair sds where
-  ds = divisors n
-  sds = take (udiv (length ds) 2) ds
-  pair x = (div n x, x)
+factorPairs n = [(a,b) | a <- [2..div n 2], (b, 0) <- [divMod n a], a >= b]
 
 termPairs :: Int -> [Pair]
-termPairs n = map pair [2..div n 2] where
-  pair x = (n-x, x)
+termPairs n = [(n-b, b) | b <- [2..div n 2]]
 
 pTs :: [Pair] -> [[Pair]]
 pTs = map (\(a, b) -> termPairs $ a+b)
@@ -28,18 +16,13 @@ pTs = map (\(a, b) -> termPairs $ a+b)
 sTp :: [Pair] -> [[Pair]]
 sTp = map (\(a, b) -> factorPairs $ a*b)
 
--- can the diolog even start?
-dialog :: [Pair] -> [Pair] -> Bool
-dialog fs ts = not . null $ intersect fs ts
-
 -- definitely cannot know
 knowNot :: [Pair] -> Bool
 knowNot ps = length ps > 1
 
 -- I know you do not know
 sKnowKnowNot :: [Pair] -> Bool
-sKnowKnowNot ts = length ts == length ns where
-  ns = filter knowNot $ sTp ts
+sKnowKnowNot ts = all knowNot $ sTp ts
 
 pKnows :: [Pair] -> Bool
 pKnows fs = 1 == length ns where
@@ -50,19 +33,17 @@ sKnows ts = 1 == length ns where
   ns = filter pKnows $ sTp ts
 
 isSolution :: Pair -> Bool
-isSolution (p, s) =
-  dialog fs ts
-  && knowNot fs
-  && knowNot ts
-  && sKnowKnowNot ts
-  && pKnows fs
-  && sKnows ts
+isSolution (a, b) =
+  and [knowNot fs
+  , knowNot ts
+  , sKnowKnowNot ts
+  , pKnows fs
+  , sKnows ts]
   where
-    fs = factorPairs p
-    ts = termPairs s
+    fs = factorPairs $ a*b
+    ts = termPairs $ a+b
 
 main :: IO ()
 main = do
-  let ((p,s):_) = filter isSolution [(p, s) | p <- [2..99], s <- [2..99]]
-      (numbers:_) = factorPairs p `intersect` termPairs s
+  let (numbers:_) = filter isSolution [(a, b) | a <- [2..99], b <- [2..99], a >= b]
   putStrLn $ "The numbers are " ++ show numbers
