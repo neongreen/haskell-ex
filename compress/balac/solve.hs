3e4ad836d18934f95fd88a8e5d9138a4e5900739
Balachandran Chandrasekharan <balachandran.c@gmail.com>
2016-09-07 10:27:18 +0530

    compress: cleanup based on Artyom's feedback.

--- compress/balac/solve.hs
+++ compress/balac/solve.hs
@@ -4,26 +4,24 @@ import Debug.Trace
 
 kMinMatch = 3
 
-sampleStr = "Consider a string. No, consider a different string. Whatever."
-
 commonPrefixLength :: Eq a => [a] -> [a] -> Int
 commonPrefixLength xs ys = length $ takeWhile ( uncurry (==) ) $ zip xs ys
 
 type Match = (Int,Int)
 
-findMatch :: String -> String -> Int -> Int -> Maybe Match
-findMatch str1 str2 startOffset endOffset
-    | startOffset == endOffset = Nothing
-    | otherwise = if cpLength < kMinMatch
-                    then findMatch ( tail str1 ) str2 ( startOffset + 1 ) endOffset
-                    else Just ( startOffset, cpLength ) 
+findMatch :: String -> String -> Int -> Maybe Match
+findMatch [] _ _ = Nothing
+findMatch str1 str2 startOffset =   
+    if cpLength < kMinMatch
+        then findMatch ( tail str1 ) str2 ( startOffset + 1 )
+        else Just ( startOffset, cpLength ) 
     where
         cpLength = commonPrefixLength str1 str2
 
 
 matchSuffixes :: [String] -> [ (Int,String) ] -> [ (Int, Match) ]
 matchSuffixes (prefix:prefixes) ((soffset,suffix):suffixes) = 
-    case findMatch prefix suffix 0 soffset of
+    case findMatch prefix suffix 0 of
         Nothing ->  matchSuffixes prefixes suffixes
         Just match@( moffset, prefixLength ) -> (soffset, match) : matchSuffixes ( drop ( prefixLength - 1 ) prefixes ) ( drop ( prefixLength - 1 ) suffixes )
 matchSuffixes _ _ = []
@@ -62,6 +60,9 @@ decompress' str ( Right (mpos,mlen) : tokens ) = decompress' ( str ++ take mlen 
 
 prop_inverse :: String -> Bool
 prop_inverse str = decompress ( compress str ) == str
+
+sampleStr1 = "Consider a string. No, consider a different string. Whatever."
+sampleStr2 = "foo|bar|foobar"
 
 main = quickCheck prop_inverse
 
