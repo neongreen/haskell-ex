64fa2302f4521c08ded1f89ac4244af4e19ce3e1
Balachandran Chandrasekharan <balachandran.c@gmail.com>
2016-09-07 10:15:01 +0530

    compress: bugfix: off-by-one error in skipping suffixes after match

--- compress/balac/solve.hs
+++ compress/balac/solve.hs
@@ -1,16 +1,13 @@
 import Data.List
 import Test.QuickCheck
+import Debug.Trace
 
 kMinMatch = 3
 
 sampleStr = "Consider a string. No, consider a different string. Whatever."
 
-commonPrefixLength :: Eq a => [a] -> [a] -> Int -> Int
-commonPrefixLength [] _ count = count
-commonPrefixLength _ [] count = count
-commonPrefixLength (x:xs) (y:ys) count = if x == y
-                                            then commonPrefixLength xs ys ( count + 1 )
-                                            else count
+commonPrefixLength :: Eq a => [a] -> [a] -> Int
+commonPrefixLength xs ys = length $ takeWhile ( uncurry (==) ) $ zip xs ys
 
 type Match = (Int,Int)
 
@@ -21,16 +18,15 @@ findMatch str1 str2 startOffset endOffset
                     then findMatch ( tail str1 ) str2 ( startOffset + 1 ) endOffset
                     else Just ( startOffset, cpLength ) 
     where
-        cpLength = commonPrefixLength str1 str2 0
+        cpLength = commonPrefixLength str1 str2
 
 
 matchSuffixes :: [String] -> [ (Int,String) ] -> [ (Int, Match) ]
-matchSuffixes [] _ = []
-matchSuffixes _ [] = []
 matchSuffixes (prefix:prefixes) ((soffset,suffix):suffixes) = 
     case findMatch prefix suffix 0 soffset of
         Nothing ->  matchSuffixes prefixes suffixes
-        Just match@( moffset, prefixLength ) -> (soffset, match) : matchSuffixes ( drop prefixLength prefixes ) ( drop prefixLength suffixes )
+        Just match@( moffset, prefixLength ) -> (soffset, match) : matchSuffixes ( drop ( prefixLength - 1 ) prefixes ) ( drop ( prefixLength - 1 ) suffixes )
+matchSuffixes _ _ = []
 
 type Token = Either String Match
 
@@ -39,7 +35,13 @@ toTokens [] _ _ = []
 toTokens str pos [] = case drop pos str of
                         "" -> []
                         rest  -> [ Left rest ]
-toTokens str pos ((offset,m@(_,prefixLength)):matches) = Left ( take ( offset - pos ) $ drop pos str ) : Right m : toTokens str ( offset + prefixLength ) matches
+toTokens str pos ((offset,m@(_,prefixLength)):matches) = 
+    if textLen > 0
+        then Left ( take textLen $ drop pos str ) : tokens
+        else tokens
+    where
+        textLen= offset - pos
+        tokens = Right m : toTokens str ( offset + prefixLength ) matches
 
 compress :: String -> [Token]
 compress "" = []
@@ -54,8 +56,8 @@ decompress = decompress' ""
 
 decompress' :: String -> [Token] -> String
 decompress' str [] = str
-decompress' str ( Left x : tokens) = decompress' ( str ++ x ) tokens
-decompress' str ( Right (mpos,mlen) : tokens) = decompress' ( str ++ take mlen ( drop mpos str ) ) tokens
+decompress' str ( Left x : tokens ) = decompress' ( str ++ x ) tokens
+decompress' str ( Right (mpos,mlen) : tokens ) = decompress' ( str ++ take mlen ( drop mpos str ) ) tokens
 
 
 prop_inverse :: String -> Bool
