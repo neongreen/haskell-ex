fb7d1b6203da36742f5a6f8e6c2b8efc41d6d546
Balachandran Chandrasekharan <balachandran.c@gmail.com>
2016-09-09 11:56:05 +0530

    jpath: apply operators.

--- jpath/balac/solve.hs
+++ jpath/balac/solve.hs
@@ -5,9 +5,13 @@ import Control.Monad (void)
 import qualified Data.ByteString.Lazy as Lazy
 import Data.Aeson
 import qualified Data.Aeson.Types as AT
+import qualified Data.HashMap.Strict as HM
 import Data.List
 import Data.Maybe
 import qualified Data.Set as S
+import qualified Data.Text as T
+import qualified Data.Vector as V
+import System.Environment
 import System.IO
 import Text.Megaparsec
 import Text.Megaparsec.Expr
@@ -106,10 +110,47 @@ readJsonFile filePath = do
             readJsonFile fileName
         Just val -> return val
 
+
+getChildByKey :: String -> Value -> AT.Parser Value
+getChildByKey key = withObject "object" $ \obj ->
+                        obj .: T.pack key
+
+getChildrenByIndices :: [Integer] -> Value -> AT.Parser Value
+getChildrenByIndices indices = withArray "array" $ \arr -> do
+                                    let maybeChildren = map ( ( arr V.!? ) . fromInteger ) indices
+                                    let children = map fromJust $ filter isJust maybeChildren
+                                    return $ Array ( V.fromList children )
+
+getAllChildren :: Value -> AT.Parser Value
+getAllChildren (Object obj) = return $ Array ( V.fromList ( HM.elems obj ) )
+
+applyOperatorsToArray :: [JPOperator] -> Value -> AT.Parser Value
+applyOperatorsToArray ops ( Array childrenVec ) = do
+    results <- mapM ( applyJPOperator ops ) childrenVec
+    return $ Array results 
+
+applyJPOperator :: [JPOperator] -> Value -> AT.Parser Value
+applyJPOperator [] value = return value
+applyJPOperator (op:ops) value = case op of
+    OpRoot                  -> applyJPOperator ops value
+    OpCurrent               -> applyJPOperator ops value
+    OpChild key             -> getChildByKey key value >>= applyJPOperator ops
+    OpSubscriptSet indices  -> getChildrenByIndices indices value >>= applyOperatorsToArray ops
+    OpAllChildren           -> getAllChildren value >>= applyOperatorsToArray ops
+
+printValue :: Value -> IO ()
+printValue = undefined
+
 main = do
     hSetEncoding stdout utf8
     hSetBuffering stdout NoBuffering
     jsonValue <- readJsonFile "store.js"
-    print jsonValue
-
-
+    args <- getArgs
+    mapM putStrLn args
+    let eJpath = runParser parseOperators "" $ head args
+    case eJpath of
+        Left err -> putStr ( parseErrorPretty err )
+        Right ops -> do
+            case AT.parseEither ( applyJPOperator ops ) jsonValue of
+                Left err -> print $ "Error: " ++ err
+                Right sel -> print sel
