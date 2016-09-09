67dc93eb8322a60dffb0fddf5ed85fba9700b945
Stefan Malewski <alviprofluvium@gmail.com>
2016-09-09 20:24:39 -0300

    mergesort: divide function changed (Artyom advice)

--- mergesort/alviprofluvium/Main.hs
+++ mergesort/alviprofluvium/Main.hs
@@ -4,8 +4,7 @@ mergeSort :: (Ord a) =>  [a] -> [a]
 mergeSort []  = []
 mergeSort [x] = [x]
 mergeSort xs  = merge (mergeSort left) (mergeSort right)
-  where (left, right) = splitAt half xs
-        half = length xs `div` 2
+  where (left, right) = divide xs
 
 merge :: (Ord a) => [a] -> [a] -> [a]
 merge [] b = b
@@ -13,3 +12,8 @@ merge a [] = a
 merge a@(x:xs) b@(y:ys)
   | x <= y     = x : merge xs b
   | otherwise  = y : merge a ys
+
+divide :: [a] -> ([a],[a])
+divide = go [] []
+  where go as bs []     = (as, bs)
+        go as bs (x:xs) = go bs (x:as) xs
