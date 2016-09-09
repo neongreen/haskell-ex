24e599e0cdb5b5af72f679eb68fd3080e9086cd0
callmecabman <pgf.hq.m@gmail.com>
2016-09-09 09:51:41 +0700

    purifying the merge sort

--- mergesort/callmecabman/Main.hs
+++ mergesort/callmecabman/Main.hs
@@ -2,23 +2,23 @@ module Main where
 
 import Data.Bifunctor
 
-merge :: Ord a => [a] -> [a] -> [a]
-merge [] ys = ys
-merge xs [] = xs
-merge s@(x:xs) t@(y:ys)
-  | x <= y = x : merge xs t
-  | x > y = y : merge s ys
+split :: [a] -> ([a], [a])
+split = split' [] []
+  where
+    split' xs ys []     = (xs, ys)
+    split' xs ys (z:zs) = split' ys (z:xs) zs
 
-mergeSort' :: Ord a => Int -> [a] -> [a]
-mergeSort' n xs
-  | n <= 1 = xs
-  | n > 1 = uncurry merge $ mapSort (splitAt k xs)
-    where
-      mapSort = bimap (mergeSort' k) (mergeSort' (n - k))
-      k = n `div` 2
+merge :: Ord a => ([a], [a]) -> [a]
+merge ([], ys) = ys
+merge (xs, []) = xs
+merge (s@(x:xs), t@(y:ys))
+  | x <= y     = x : merge (xs, t)
+  | otherwise = y : merge (s, ys)
 
 mergeSort :: Ord a => [a] -> [a]
-mergeSort xs = mergeSort' (length xs) xs
+mergeSort []  = []
+mergeSort [x] = [x]
+mergeSort xs  = merge $ bimap mergeSort mergeSort $ split xs
 
 main :: IO ()
 main = return ()
