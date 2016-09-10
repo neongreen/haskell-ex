8afd23c7c8c80400348d17c75e41b5427e646d53
Jason Kuhrt <jasonkuhrt@me.com>
2016-09-09 23:29:26 -0400

    compress: Refactor. Again.

--- compress/jasonkuhrt/source/Compress.hs
+++ compress/jasonkuhrt/source/Compress.hs
@@ -4,97 +4,16 @@
 -- to the first instance. Performance is not a goal.
 
 
+{- ALGORITHM SKETCH/EXAMPLE
 
-module Compress where
+* Terminology
 
+"|" is a cursor, used to track progress on "track" which is the
+combination of what is behind and ahead of cursor, e.g.:
 
-
-type Compressed = [CompressedChunk]
-type CompressedChunk = Either String CompressionRef
-type CompressionRef = (Int, Int)
-type Track = (String, String)
-
-
-
--- Undoing Compression --
-
-remove :: Compressed -> String
-remove = foldl go "" where
-
-  go :: String -> CompressedChunk -> String
-  go uncompressed (Right ref)   = deRef uncompressed ref
-  go uncompressed (Left string) = uncompressed ++ string
-
-  deRef :: String -> CompressionRef -> String
-  deRef uncompressed (i, size) = uncompressed ++ takeSlice i size uncompressed
-
-
-
--- Doing Compression --
-
-put :: String -> Compressed
-put ""     = []
-put string = go (-1) 0 where
-
-  end = length string
-
-  go :: Int -> Int -> Compressed
-  go mmsi progress -- mmsi = maybeMatchStartIndex
-    | end == progress    = []
-    | end - progress < 3 = [Left trackAhead]
-    | mmsi >= 0          =
-      case matchScan track of
-      Nothing  -> [Left trackAhead]
-      Just ref ->
-        let (i, size) = ref
-        in Right ref : go (-1) (progress + size)
-    | otherwise          =
-      let (matchStart, size) = advanceWhileMatchNot track
-      in  Left (take size trackAhead) : go matchStart (progress + size)
-    where
-    track = splitAt progress string
-    (_, trackAhead) = track
-
-
-
-advanceWhileMatchNot :: Track -> (Int, Int)
-advanceWhileMatchNot = go 0 where
-
-  go size (_, [])               = (-1, size)
-  go size track@(behind, ahead) =
-    case findMatchStart behind (take 3 ahead) of
-      Nothing         -> go (size + 1) (trackAdvance track)
-      Just matchStart -> (matchStart, size)
-
-  trackAdvance (behind, a:as) = (behind ++ [a], as)
-
-
-
-matchScan :: Track -> Maybe CompressionRef
-matchScan (behind, ahead) =
-  go Nothing (take 3 ahead)
-  where
-  go lastMatch prospect =
-    case findMatchStart behind prospect of
-      Nothing ->
-        lastMatch
-      Just i  ->
-        let prospectSize = length prospect
-            newMatch = Just (i, prospectSize)
-        in if elem prospectSize [length behind, length ahead]
-           then newMatch
-           else go newMatch (take (prospectSize + 1) ahead)
-
-
-
-
-
-{-
-| is a cursor (use to track progress on track)
 behind|ahead
 
-
-Given "foofooxfoox"
+Example: Given "foofooxfoox"
 
 * If string less than six characters we cannot compress.
 * Advance cursor to after min compressable size
@@ -158,6 +77,96 @@ TRACK foofooxfoox|
 * When cursor index is at end DONE!
 
 -}
+
+-- TODO function docs
+
+module Compress where
+
+
+
+type Compressed = [CompressedChunk]
+type CompressedChunk = Either String CompressionRef
+type CompressionRef = (Int, Int)
+type Track = (String, String)
+
+
+
+-- Undoing Compression --
+
+remove :: Compressed -> String
+remove = foldl go "" where
+
+  go :: String -> CompressedChunk -> String
+  go uncompressed (Right ref)   = deRef uncompressed ref
+  go uncompressed (Left string) = uncompressed ++ string
+
+  deRef :: String -> CompressionRef -> String
+  deRef uncompressed (i, size) = uncompressed ++ takeSlice i size uncompressed
+
+
+
+-- Doing Compression --
+
+put :: String -> Compressed
+put string = go (makeTrack string) where
+
+  go :: Track -> Compressed
+  go track@(_,ahead)
+    | null ahead       = []
+    | length ahead < 3 = [Left ahead]
+    | otherwise        =
+      case tryCompressingChunk track of
+      Nothing ->
+        let size = measureUncompressableChunk track
+        in  Left (take size ahead) : go (advanceCursor size track)
+      Just ref ->
+        let (i, size) = ref
+        in Right ref : go (advanceCursor size track)
+
+
+
+makeTrack = (,) ""
+
+advanceCursor :: Int -> Track -> Track
+advanceCursor n (behind, ahead) =
+  (behind ++ take n ahead, drop n ahead)
+
+
+-- NOTE Track MUST have 3+ ahead
+-- NOTE that "3" is the minimum compressable size
+measureUncompressableChunk :: Track -> Int
+measureUncompressableChunk = go 0 where
+  go size (_, [])               = size
+  go size track@(behind, ahead) =
+    case findMatchStart behind (take 3 ahead) of
+      Nothing -> go (size + 1) (advanceCursor 1 track)
+      Just _  -> size
+
+
+
+-- NOTE Track MUST have 3+ ahead
+-- NOTE that "3" is the minimum compressable size
+tryCompressingChunk :: Track -> Maybe CompressionRef
+tryCompressingChunk (behind, ahead) =
+  go Nothing (take 3 ahead) where
+
+  go :: Maybe CompressionRef -> String -> Maybe CompressionRef
+  go lastMatch prospect =
+    case findMatchStart behind prospect of
+    Nothing ->
+      lastMatch
+    Just i  ->
+      let prospectSize = length prospect
+          newMatch = Just (i, prospectSize)
+      in if elem prospectSize [length behind, length ahead]
+         then newMatch
+         else go newMatch (take (prospectSize + 1) ahead)
+
+
+
+
+
+
 
 -- General Helpers --
 
