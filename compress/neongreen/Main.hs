b90d8c49505c0b70fcfc3d0d9b40fb512cc7de47
Artyom <yom@artyom.me>
2016-09-10 17:01:32 +0300

    [compress] Better tests, more simplification

--- compress/neongreen/Main.hs
+++ compress/neongreen/Main.hs
@@ -4,7 +4,14 @@ import Data.Either
 import Test.QuickCheck
 
 main = do
-  quickCheck $ \xs -> decompress (compress xs) === xs
+  let prop = \xs -> decompress (compress xs) === xs
+  quickCheck prop
+  quickCheck $ forAllShrink genStr shrink prop
+
+genStr :: Gen String
+genStr = concat <$> listOf (oneof [part, arbitrary])
+  where
+    part = growingElements ["a","bc","def","ghij","klmno","pqrstu"]
 
 type Token = Either String (Int, Int)  -- Starting position, length
 
@@ -40,29 +47,26 @@ Consolidate all characters found by 'compress' into strings.
 [Left "xy", Right (0,3)]
 -}
 consolidate :: [Token] -> [Token]
-consolidate [] = []
-consolidate xs =
-  let (lefts, rest) = span isLeft xs
-      leftsString = concat [x | Left x <- lefts]
-  in  case (lefts, rest) of
-        ([], Right r : rs) -> Right r : consolidate rs
-        (_ , Right r : rs) -> Left leftsString : Right r : consolidate rs
-        (_ , [])           -> [Left leftsString]
+consolidate []             = []
+consolidate (Right x : xs) = Right x : consolidate xs
+consolidate xs             = Left (concat (lefts ls)) : consolidate rest
+  where (ls, rest) = span isLeft xs
 
 compress :: String -> [Token]
-compress s = consolidate $ go [] s
+compress s = consolidate $ go "" s
   where
     -- The first parameter is the string we've already processed,
     -- the second – string left to process
     go :: String -> String -> [Token]
-    go _ [] = []
-    go p (c:s) =
-      let (i,n) = longestMatch p (c:s)
-          -- we can use @splitAt n@ because the match is always a prefix of
-          -- the second string (i.e. @c:s@)
-          (match, rest) = splitAt n (c:s)
-      in if n < 3 then Left [c]    : go (p ++ [c]) s
-                  else Right (i,n) : go (p ++ match) rest
+    go _ "" = []
+    go p (c:cs)
+      | n < 3     = Left [c]    : go (p ++ [c]) cs
+      | otherwise = Right (i,n) : go (p ++ match) rest
+      where
+        (i,n) = longestMatch p (c:cs)
+        -- we can use @splitAt n@ because the match is always a prefix of
+        -- the second string (i.e. @c:s@)
+        (match, rest) = splitAt n (c:cs)
 
 decompress :: [Token] -> String
 decompress xs = uncompressed
