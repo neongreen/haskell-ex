3af646485b5b2247ea5d2a9529d054c41dd6a32d
Stefan Malewski <alviprofluvium@gmail.com>
2016-09-10 18:37:21 -0300

    compress: works but horribly inefficient

--- compress/alviprofluvium/Main.hs
+++ compress/alviprofluvium/Main.hs
@@ -18,8 +18,35 @@ subStrings :: String -> [String]
 subStrings xs =  reverse $ sortOn length viableSubstrings
   where 
     long x           = length x > 2
-    viableSubstrings = filter (`isInfixOf` xs) $ filter long $ nub $ subsequences xs
+    viableSubstrings = filter (`isInfixOf` xs) $ filter long $ subsequences xs
 
 
 findSubstringIndex :: String -> String -> Maybe Int
 findSubstringIndex pat str = findIndex (isPrefixOf pat) $ tails str
+
+loop :: String -> String -> [Code]
+loop _ [] = []
+loop comp uncomp@(x:xs) 
+  | isNothing compresable = Left [x] : loop (comp ++ [x]) xs
+  | otherwise             = Right (fromJust compresable) : loop (comp ++ take len uncomp) (drop len uncomp)
+  where
+    compresable = tryCompress uncomp comp
+    (_, len)    = fromJust compresable
+
+compress :: String -> [Code]
+compress = flatten . loop []
+
+flatten :: [Code] -> [Code]
+flatten [] = []
+flatten (Left x:Left y:zs) = flatten $ Left (x++y) : zs
+flatten (x:xs) = x : flatten xs
+
+uncompressLoop :: [Code] -> String -> String
+uncompressLoop [] ys = ys
+uncompressLoop (Right (index, length):xs) ys = uncompressLoop xs $ ys ++ link 
+  where link = take length $ drop index ys
+uncompressLoop (Left x:xs) ys = uncompressLoop xs $ ys ++ x
+
+uncompress :: [Code] -> String
+uncompress xs = uncompressLoop xs []
+
