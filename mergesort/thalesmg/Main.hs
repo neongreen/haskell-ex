793f9d765b1834426812b0531154d41e4bce525b
Thales Macedo Garitezi <thalesmg@gmail.com>
2016-09-10 09:42:42 -0300

    Working mergesort, but broken main.

---
+++ mergesort/thalesmg/Main.hs
@@ -0,0 +1,25 @@
+import Control.Monad
+
+merge :: (Ord a) => [a] -> [a] -> [a]
+merge [] [] = []
+merge [] lstB = lstB
+merge lstA [] = lstA
+merge lstA@(a:as) lstB@(b:bs) = if a <= b then a : merge as lstB
+                                          else b : merge lstA bs
+                                          
+split :: (Ord a) => [a] -> ([a], [a])
+split lst = splitAt (length lst `div` 2) lst
+
+mergesort :: (Ord a) => [a] -> [a]
+mergesort [] = []
+mergesort [x] = [x]
+mergesort lst =
+  merge (mergesort lstA) (mergesort lstB)
+  where
+    (lstA, lstB) = split lst
+
+main :: IO ()
+main = do
+  xs <- lines <$> getContents >>= return . map (read :: String -> Int)
+  -- print xs
+  mapM_ putStrLn (mergesort xs)
