1b30761041d04ea6e654c74b0c4bd916bbfeb994
Stefan Malewski <alviprofluvium@gmail.com>
2016-09-10 22:56:56 -0300

    compress: changed substring match function

--- compress/alviprofluvium/Main.hs
+++ compress/alviprofluvium/Main.hs
@@ -1,24 +1,31 @@
 import Data.List
 import Data.Ord
 import Data.Maybe
+import Data.Function
 
 type Patch = (Int, Int)
 type Code = Either String Patch
 
 tryCompress :: String -> String -> Maybe Patch
-tryCompress xs ys = createPatch match
+tryCompress xs ys = createPatch $ matches xs ys
   where 
-    match = find (`isPrefixOf` xs) $ subStrings ys
     createPatch :: Maybe String -> Maybe Patch
     createPatch Nothing  = Nothing
     createPatch (Just x) = Just (index, length x)
       where index = fromJust $ findSubstringIndex x ys
 
-subStrings :: String -> [String]
-subStrings xs =  reverse $ sortOn length viableSubstrings
+matches :: String -> String -> Maybe String
+matches xs ys = find (`isPrefixOf` xs) viableSubstrings
   where 
-    long x           = length x > 2
-    viableSubstrings = filter (`isInfixOf` xs) $ filter long $ subsequences xs
+    long x = length x > 2
+    viableSubstrings = reverse $ sortOn length $ filter long $ getSubstrings xs ys
+
+--Source
+--https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Longest_common_substring#Haskell
+getSubstrings :: String -> String -> [String]
+getSubstrings xs ys = concat $ [f xs' ys | xs'<- tails xs] ++ [f xs ys' | ys' <- drop 1 $ tails ys]
+  where f xs ys = scanl g [] $ zip xs ys
+        g z (x,y) = if x == y then z ++ [x] else []
 
 
 findSubstringIndex :: String -> String -> Maybe Int
@@ -27,11 +34,11 @@ findSubstringIndex pat str = findIndex (isPrefixOf pat) $ tails str
 loop :: String -> String -> [Code]
 loop _ [] = []
 loop comp uncomp@(x:xs) 
-  | isNothing compresable = Left [x] : loop (comp ++ [x]) xs
-  | otherwise             = Right (fromJust compresable) : loop (comp ++ take len uncomp) (drop len uncomp)
+  | isNothing compression = Left [x] : loop (comp ++ [x]) xs
+  | otherwise             = Right (fromJust compression) : loop (comp ++ take len uncomp) (drop len uncomp)
   where
-    compresable = tryCompress uncomp comp
-    (_, len)    = fromJust compresable
+    compression = tryCompress uncomp comp
+    (_, len)    = fromJust compression
 
 compress :: String -> [Code]
 compress = flatten . loop []
@@ -49,4 +56,7 @@ uncompressLoop (Left x:xs) ys = uncompressLoop xs $ ys ++ x
 
 uncompress :: [Code] -> String
 uncompress xs = uncompressLoop xs []
+
+testCompression :: String -> Bool
+testCompression xs = (uncompress . compress) xs == xs
 
