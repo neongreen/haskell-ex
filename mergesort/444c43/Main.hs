e30a8f383cb7e8a4c3da6dd4eceabafc3a2ca1e4
DL <444c43@gmail.com>
2016-09-11 00:14:53 -0400

    merge : add merge

--- mergesort/444c43/Main.hs
+++ mergesort/444c43/Main.hs
@@ -1,2 +1,8 @@
+merge xs [] = xs
+merge [] ys = ys
+merge (x:xs) (y:ys)
+  | x <= y = x : merge xs (y:ys)
+  | y <= x = y : merge (x:xs) ys
+
 splitlist :: [a] -> ([a], [a])
 splitlist list = splitAt ((length (list) + 1) `div` 2) list
