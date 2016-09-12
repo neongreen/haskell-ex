d1a444e0a19fbb012e5575a97634844d52858959
Stefan Malewski <alviprofluvium@gmail.com>
2016-09-12 15:09:06 -0300

    expr: showExpr without parens

--- expr/alviprofluvium/Expr.hs
+++ expr/alviprofluvium/Expr.hs
@@ -8,7 +8,13 @@ data Expr
   | Div Expr Expr
 
 showExpr :: Expr -> String
-showExpr _ = ""
+showExpr (Number n)
+  | n < 0     = "(" ++ show n ++ ")"
+  | otherwise = show n
+showExpr (Add a b) = showExpr a ++ "+" ++ showExpr b
+showExpr (Sub a b) = showExpr a ++ "-" ++ showExpr b
+showExpr (Mul a b) = showExpr a ++ "*" ++ showExpr b
+showExpr (Div a b) = showExpr a ++ "/" ++ showExpr b
 
 evalExpr :: Expr -> Int
 evalExpr (Number n) = n
@@ -16,3 +22,5 @@ evalExpr (Add a b)  = evalExpr a + evalExpr b
 evalExpr (Sub a b)  = evalExpr a - evalExpr b
 evalExpr (Mul a b)  = evalExpr a * evalExpr b
 evalExpr (Div a b)  = evalExpr a `div` evalExpr b
+
+
