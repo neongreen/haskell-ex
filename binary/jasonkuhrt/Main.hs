d4ea20f2d01051f306bbe1b21d39f8c4198e87dd
Jason Kuhrt <jasonkuhrt@me.com>
2016-09-13 10:36:12 -0400

    binary: Scaffolding.

---
+++ binary/jasonkuhrt/Main.hs
@@ -0,0 +1,23 @@
+
+{- #  Binary Conversion
+      Convert a number to binary and back.
+
+  ##  Example
+
+      > bin 123
+      "1111011"
+
+      > dec "1111011"
+      123
+-}
+
+
+
+module Main where
+
+import qualified Test.QuickCheck as QS
+
+
+
+main :: IO ()
+main = putStrLn "Hello World"
