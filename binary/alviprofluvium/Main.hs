d14529978cf358de2847a9ccd61ededd74acfc83
Stefan Malewski <alviprofluvium@gmail.com>
2016-09-13 09:52:14 -0300

    binary: implemented Artyom suggestions

--- binary/alviprofluvium/Main.hs
+++ binary/alviprofluvium/Main.hs
@@ -8,21 +8,18 @@ bin n = go n ""
   where
     go :: Natural -> String -> String
     go 0 bs = bs
-    go n bs 
-      | even n = go (div n 2) $ '0' : bs
-      | otherwise = go (div n 2) $ '1' : bs
+    go n bs = go half $ (intToDigit . fromEnum) rest : bs
+      where
+        (half, rest) = divMod n 2
 
 dec :: String -> Natural
 dec = sum . zipWith toPower powersOfTwo . reverse
   where
     toPower :: Natural -> Char -> Natural
-    toPower pow dig = pow * toNatural (digitToInt dig)
+    toPower pow dig = pow * toEnum (digitToInt dig)
 
     powersOfTwo :: [Natural]
     powersOfTwo = iterate (*2) 1
-
-    toNatural :: Int -> Natural
-    toNatural = fromInteger . toInteger
 
 procBinary :: Natural -> Property
 procBinary n = (dec . bin) n === n
