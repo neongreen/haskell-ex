98f5c92e6efcc3c42ba23d6f5d50416030aded2f
DL <444c43@gmail.com>
2016-09-12 20:35:54 -0400

    rename function

--- binary/444c43/Main.hs
+++ binary/444c43/Main.hs
@@ -3,6 +3,6 @@ import Data.Char
 toBinary :: Int -> [Char]
 toBinary a = map intToDigit(convertDecimal a)
 
-convertDecimal :: Integral a => a -> [a]
-convertDecimal 0 = []
-convertDecimal a = convertDecimal(a `div` 2) ++ [a `mod` 2]
+divisionByTwo :: Integral a => a -> [a]
+divisionByTwo 0 = []
+divisionByTwo a = convertDecimal(a `div` 2) ++ [a `mod` 2]
