4109d5d0446e2ba05b49ccf2295721325733f9c9
DL <444c43@gmail.com>
2016-09-13 08:00:23 -0400

    binary : convert binary string to Int

--- binary/444c43/Main.hs
+++ binary/444c43/Main.hs
@@ -7,3 +7,6 @@ toBinary a = map intToDigit(divisionByTwo a)
 divisionByTwo :: Integral a => a -> [a]
 divisionByTwo 0 = []
 divisionByTwo a = divisionByTwo(a `div` 2) ++ [a `mod` 2]
+
+toInt :: String -> Int
+toInt = foldl' (\acc x -> acc * 2 + digitToInt x) 0
