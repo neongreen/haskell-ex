a9a7404dd79dbb142d762fbd3f17a635d387cfac
Jason Kuhrt <jasonkuhrt@me.com>
2016-09-16 11:26:48 -0400

    expr: Refactor. Done!

--- expr/jasonkuhrt/Main.hs
+++ expr/jasonkuhrt/Main.hs
@@ -24,27 +24,23 @@ import qualified Text.Printf as Print
 
 main :: IO ()
 main = do
-  print "Should have parens"
-  putStrLn . exprShow $ Sub (Number 2) (Sub (Number 2) (Number 1))
-  putStrLn . exprShow $ Div (Number 2) (Sub (Number 2) (Number 1))
-  putStrLn . exprShow $ Div (Sub (Number 2) (Number 1)) (Number 2)
-  putStrLn . exprShow $ example
-  print . eval $ example
-  print "Should not have any parens"
-  putStrLn . exprShow $ Sub (Sub (Number 2) (Number 2)) (Number 1)
-  putStrLn . exprShow $ Sub (Number 2) (Div (Number 2) (Number 1))
-  putStrLn . exprShow $ Sub (Div (Number 2) (Number 1)) (Number 2)
+  putStrLn "\n# Required\n"
+  putStrLn . visualize $ Sub (Number 2) (Sub (Number 2) (Number 1))
+  putStrLn . visualize $ Div (Number 2) (Sub (Number 2) (Number 1))
+  putStrLn . visualize $ Div (Sub (Number 2) (Number 1)) (Number 2)
+  putStrLn $ visualize example ++ " == " ++ (show . eval) example
+  putStrLn "\n# Optional\n"
+  putStrLn . visualize $ Sub (Sub (Number 2) (Number 2)) (Number 1)
+  putStrLn . visualize $ Sub (Number 2) (Div (Number 2) (Number 1))
+  putStrLn . visualize $ Sub (Div (Number 2) (Number 1)) (Number 2)
 
 example :: Expr
 example =
   Mul (Number 3)
-       (Add (Number 5)
-            (Div
-              (Sub (Number 7)
-                   (Number 1))
-              (Number 2)))
-
-
+      (Add (Number 5)
+           (Div (Sub (Number 7)
+                     (Number 1))
+                 (Number 2)))
 
 data Expr
   = Number Int
@@ -53,22 +49,27 @@ data Expr
   | Mul Expr Expr
   | Div Expr Expr
 
+
+
+-- Evaluate An Expression --
+
 eval :: Expr -> Int
 eval (Number int)  = int
 eval (Mul ex1 ex2) = (*) (eval ex1) (eval ex2)
 eval (Add ex1 ex2) = (+) (eval ex1) (eval ex2)
 eval (Div ex1 ex2) = div (eval ex1) (eval ex2)
-eval (Sub ex1 ex2) = (-) (eval ex2) (eval ex1) -- Yes, arguments flipped
+eval (Sub ex1 ex2) = (-) (eval ex1) (eval ex2)
 
 
+-- Visualize An Expression --
 
 {- Considering need-only parens
 
 There are two factors influencing the evaluation order of an expression.
 
-## Operator Precedence
+## Operator opPrecedence
 
-Operator precedence assigns different priority to different groups of operators. The following is the abbreviated hierarchy, suitable for our needs:
+Operator opPrecedence assigns different priority to different groups of operators. The following is the abbreviated hierarchy, suitable for our needs:
 
         ^
        * +
@@ -84,18 +85,18 @@ For example in the following some parens are _required_ for eval order, while ot
 
 ## Associativity
 
-Operators of equal precedence are evaluated in order of associativity: left-hand-side (lhs) or right-hand side (rhs). Usually operators are lhs; "^*+/-" all are. Consequently when a computation using these operators needs right-before-left then parens are required, but conversely in left-before-right they are optional.
+Operators of equal opPrecedence are evaluated in order of associativity: left-hand-side (lhs) or right-hand side (rhs). Usually operators are lhs; "^*+/-" all are. Consequently when a computation using these operators needs right-before-left then parens are required, but conversely in left-before-right they are optional.
 
     Required          Optional
     --------          --------
     2 - (2 - 1)       (2 - 2) - 1  ==  2 - 2 - 1
     2 / (4 * 2)       (2 / 4) * 2  ==  2 / 4 * 2
 
-There are two common reasons to use parens: Firstly, for correctness when the operator precedence or associativity does not conform to the computation's needs; Secondly, for readability, when otherwise remembering the hierarchy/associativity (plus mapping it onto the expression) could be mentally taxing, thus error prone.
+There are two common reasons to use parens: Firstly, for correctness when the operator opPrecedence or associativity does not conform to the computation's needs; Secondly, for readability, when otherwise remembering the hierarchy/associativity (plus mapping it onto the expression) could be mentally taxing, thus error prone.
 
 With this knowledge logic of when to use parens is as follows:
 
-    if exp op is _lower_ precedence than parent-exp op
+    if exp op is _lower_ opPrecedence than parent-exp op
       REQUIRED
     if exp is RHS &&
       if exp op is _same_ as parent-exp op &&
@@ -105,34 +106,58 @@ With this knowledge logic of when to use parens is as follows:
       OPTIONAL
 -}
 
-lazyParens :: Int -> Expr -> Expr -> String -> String
-lazyParens side parent c result
-  | (side == 1 && isSameOp parent c && (not . isAssocOp) c ) ||
-    isLowerPrecedence c parent =
-    Print.printf "(%s)" result
-  | otherwise = result
-
-exprShow :: Expr -> String
-exprShow expr =
-  Print.printf "%s %s %s"
-  (go 0 expr (left expr))
-  (exprOpShow expr)
-  (go 1 expr (right expr))
+visualize :: Expr -> String
+visualize (Number n) = show n
+visualize p          =
+  Print.printf "%s %s %s" goLeft (op p) goRight
   where
-  go :: Int -> Expr -> Expr -> String
-  go _ _ (Number n) = show n
-  go 1 parent e =
-    lazyParens 1 parent e $ exprShow e
-  go 0 parent e =
-    lazyParens 0 parent e $ exprShow e
-  go _ _ _ = undefined
+  goLeft  = ifthen (needsParensLHS p) parenify . visualize $ left p
+  goRight = ifthen (needsParensRHS p) parenify . visualize $ right p
 
-exprOpShow :: Expr -> String
-exprOpShow (Mul _ _)  = "*"
-exprOpShow (Div _ _)  = "/"
-exprOpShow (Sub _ _)  = "-"
-exprOpShow (Add _ _)  = "+"
-exprOpShow (Number _) = ""
+  op (Mul _ _)  = "*"
+  op (Div _ _)  = "/"
+  op (Sub _ _)  = "-"
+  op (Add _ _)  = "+"
+  op _          = "" -- NOTE Impossible case. See parent pattern matches.
+
+
+needsParensRHS :: Expr -> Bool
+needsParensRHS p = let c = right p in
+  opPrecedence p > opPrecedence c ||
+  (isOpSame p c && (not . isOpAssoc) c)
+
+needsParensLHS :: Expr -> Bool
+needsParensLHS p =
+  opPrecedence p > opPrecedence (left p)
+
+
+opPrecedence :: Expr -> Int
+opPrecedence (Number _) = 100
+opPrecedence (Mul _ _)  = 9
+opPrecedence (Div _ _)  = 9
+opPrecedence (Sub _ _)  = 8
+opPrecedence (Add _ _)  = 8
+
+
+{- | Is the operator of a given expression associative?
+
+Certain operators are associative. An associative operator is one whose evaluation order is insignificant. For example the operator `+` is associative so the expression `1 + 2 + 3` it can be evaluated in either order: `1 + (2 + 3)` or `(1 + 2) + 3`. It is insignificant.
+
+A use-case for this information is when, in the act of visualizing an expression tree, deciding whether certain parts needs parens to be correct. -}
+isOpAssoc :: Expr -> Bool
+isOpAssoc (Mul _ _) = True
+isOpAssoc (Add _ _) = True
+isOpAssoc _         = False
+
+
+{- | Do two given expressions use the same operators? -}
+isOpSame :: Expr -> Expr -> Bool
+isOpSame (Mul _ _) (Mul _ _) = True
+isOpSame (Div _ _) (Div _ _) = True
+isOpSame (Add _ _) (Add _ _) = True
+isOpSame (Sub _ _) (Sub _ _) = True
+isOpSame _         _         = False
+
 
 left :: Expr -> Expr
 left (Mul x _) = x
@@ -148,25 +173,13 @@ right (Sub _ x) = x
 right (Add _ x) = x
 right (Number x) = Number x
 
-isLowerPrecedence :: Expr -> Expr -> Bool
-isLowerPrecedence (Mul _ _) (Add _ _) = False
-isLowerPrecedence (Mul _ _) (Sub _ _) = False
-isLowerPrecedence (Div _ _) (Add _ _) = False
-isLowerPrecedence (Div _ _) (Sub _ _) = False
-isLowerPrecedence (Div _ _) (Div _ _) = False
-isLowerPrecedence (Mul _ _) (Mul _ _) = False
-isLowerPrecedence (Add _ _) (Add _ _) = False
-isLowerPrecedence (Sub _ _) (Sub _ _) = False
-isLowerPrecedence _         _         = True
 
-isAssocOp :: Expr -> Bool
-isAssocOp (Mul _ _) = True
-isAssocOp (Add _ _) = True
-isAssocOp _         = False
 
-isSameOp :: Expr -> Expr -> Bool
-isSameOp (Mul _ _) (Mul _ _) = True
-isSameOp (Div _ _) (Div _ _) = True
-isSameOp (Add _ _) (Add _ _) = True
-isSameOp (Sub _ _) (Sub _ _) = True
-isSameOp _         _         = False
+-- General Helpers --
+
+parenify :: String -> String
+parenify = Print.printf "(%s)"
+
+ifthen :: Bool -> (a -> a) -> a -> a
+ifthen True  f val = f val
+ifthen False _ val = val
