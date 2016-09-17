4c04a01b87e7e9555f1ceedf0f09049b10d7b97a
Jason Kuhrt <jasonkuhrt@me.com>
2016-09-16 21:51:08 -0400

    expr: Tidy. Docs.

--- expr/jasonkuhrt/Main.hs
+++ expr/jasonkuhrt/Main.hs
@@ -1,8 +1,8 @@
 {- README
 
-  # Expr
+--- Expr
 
- ## Goal
+ -- Goal
 
     * With a data model for arithmatic, write functions to
       print and evaluate expressions.
@@ -19,6 +19,7 @@ main :: IO ()
 main = do
   putStrLn "\n# Required\n"
   putStrLn . visualize $ Sub (Number 1) (Add (Number 2) (Number 3))
+  putStrLn . visualize $ Add (Number 1) (Sub (Number 2) (Number 3))
   putStrLn . visualize $ Div (Number 10) (Number (-1))
   putStrLn . visualize $ Sub (Number 2) (Sub (Number 2) (Number 1))
   putStrLn . visualize $ Div (Number 2) (Sub (Number 2) (Number 1))
@@ -35,7 +36,7 @@ example =
       (Add (Number 5)
            (Div (Sub (Number 7)
                      (Number 1))
-                 (Number 2)))
+                 (Number (-2))))
 
 data Expr
   = Number Int
@@ -63,21 +64,23 @@ eval (Sub l r)    = (-) (eval l) (eval r)
 
 Parenthesis will only be used where necessary.
 
+> visualize (Sub (Number 1) (Add (Number 2) (Number 3)))
+"1 - (2 + 3)"
+
 > visualize (Mul (Number 3) (Add (Number 5) (Number 7)))
 "3 * (5 + 7)"
 
-> visualize Mul (Number (-3))
-                (Add (Number 5) (Div (Sub (Number 7) (Number 1)) (Number 2)))
+> visualize (Mul (Number (-3)) (Add (Number 5) (Div (Sub (Number 7) (Number 1)) (Number 2))))
 "(-3) * (5 + (7 - 1) / 2)"
 
 
 
-  # Solution Notes
+--- Solution Notes
 
     The hard part is need-only parens. Our solution must model the two
     factors that influence the evaluation order of expressions.
 
- ## Factor 1: Operator Precedence
+--- Factor 1: Operator Precedence
 
     Operator precedence assigns different priority to different groups of
     operators. The following is the abbreviated hierarchy, suitable for our
@@ -95,35 +98,53 @@ Parenthesis will only be used where necessary.
         (2 + 2) * 2       2 + (2 * 2)  ==  2 + 2 * 2
         (4 - 4) / 2       4 - (4 / 2)  ==  4 - 4 / 2
 
- ## Factor 2: Associativity
+--- Factor 2: Associativity
 
-    Operators of equal precedence are evaluated in order of associativity:
-    left-hand-side (lhs) or right-hand side (rhs). Usually operators are
-    lhs; ^ * + / - all are. Consequently when a computation using these
-    operators needs right-before-left then _parens are required_, but
-    conversely in left-before-right they are optional.
+    Operators of equal precedence are evaluated in order of their left or
+    right associativity. Usually operators are left-associative
+    (^ * + / - all are). When a computation using a left-associative operator
+    has a nested right expression or vice-versa (left expression in a
+    right-associative operator) then parens will be required, which one exception.
 
         Required          Optional
         --------          --------
         2 - (2 - 1)       (2 - 2) - 1  ==  2 - 2 - 1
         2 / (4 * 2)       (2 / 4) * 2  ==  2 / 4 * 2
 
- ## Conclusion
+    The exception is fully-associative parent operators wherein parenthesis
+    are not required! Consider:
+
+                          1 + (2 - 1)  ==  1 + 2 - 1  ==  2
+
+    The reason for this case might be more clear when subtraction is
+    made into explicit addition of negative numbers:
+
+        SAME!
+        1+2-1    ->  1 +2 +(-1)
+        1+(2-1)  ->  1 +2 +(-1)
+
+    But again with left-associative parent
+
+      DIFFERENT!
+      1-(2+1)  ->  1 +(-2) +(-1)
+      1-2+1    ->  1 +(-2) +1
+
+--- Conclusion
 
     With this knowledge, the logic of when to use parens is:
 
         if: exp op is _lower_ precedence than parent-exp op
           REQUIRED
-        if: parent-exp op is _not_ associative
-          REQUIRED
-        if: exp is RHS
-          if: exp op precedence equal to parent-exp op precedence
-            if: parent-exp op is _not_ associtive
-              REQUIRED
+        if: parent-exp op is LA (_not_ fully associative)
+          if: right child-exp
+            REQUIRED TODO
+        if: parent-exp op is RA (_not_ fully associative)
+          if: left child-exp
+            REQUIRED TODO
         else
           OPTIONAL
 
- ## Aside
+--- Aside
 
     There are two common reasons to use parentheses: Firstly, for correctness
     when the operator precedence or operator associativity does not conform
@@ -143,14 +164,15 @@ visualize p          =
   op (Div _ _)  = "/"
   op (Sub _ _)  = "-"
   op (Add _ _)  = "+"
-  op _          = "" -- NOTE Impossible case. See parent pattern matches.
+  -- Impossible case. See parent pattern matches.
+  op _          = error "No operator for leaves"
 
 
+-- TODO Correct LA/R RA/L paren requirement. See comments above
 needsParensRHS :: Expr -> Bool
 needsParensRHS p = let c = right p in
-     opPrecedence p > opPrecedence c
-  || opPrecedence p == opPrecedence c &&
-     (not . isOpAssoc) p
+     opPrecedence p  > opPrecedence c
+  || opPrecedence p == opPrecedence c && (not . isOpAssoc) p
 
 needsParensLHS :: Expr -> Bool
 needsParensLHS p =
