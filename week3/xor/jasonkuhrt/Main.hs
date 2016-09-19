0169558a98e051c7a56c43666f03100a7fd8d8bf
Jason Kuhrt <jasonkuhrt@me.com>
2016-09-19 13:14:56 -0400

    xor: Implement file encryption.

--- week3/xor/jasonkuhrt/Main.hs
+++ week3/xor/jasonkuhrt/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 {- README
 
 -- XOR Encryption
@@ -22,46 +23,63 @@ Due to the way XOR works, encrypting an already encrypted file will decrypt
 it. Learn more about XOR Ciphers at en.wikipedia.org/wiki/xor_cipher.
 -}
 
+
+
 module Main where
 
-import Data.ByteString (ByteString)
-import qualified Data.ByteString as B
 import qualified Data.Bits as Bits
-import qualified Data.ByteString.Char8 as BC8
+import qualified System.Environment as Env
+import Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.Lazy.Char8 as BC8
+import Data.Text.Lazy (Text)
+import qualified Data.Text.Lazy as Text
+import qualified Data.Text.Lazy.Encoding as Enc
 
 
 
 {- | Encrypt a string via key+XOR+key. -}
-encrypt :: String -> ByteString -> ByteString
-encrypt key bs =
-  B.pack (B.zipWith Bits.xor (BC8.pack cycledKey) bs)
-  where
-  cycledKey = take (B.length bs) (cycle key)
+encrypt :: ByteString -> ByteString -> ByteString
+encrypt key contents =
+  B.pack (B.zipWith Bits.xor (B.cycle key) contents)
 
-{- | Decrypt a string via key+XOR. -}
-decrypt :: String -> ByteString -> String
-decrypt key bs = BC8.unpack (encrypt key bs)
+{- | Decrypt a string via key+XOR.
+
+Because of how XOR works decrypt is just `encrypt` run on something
+already encrypted. -}
+decrypt :: ByteString -> ByteString -> ByteString
+decrypt = encrypt
 
 
 
 test :: IO ()
 test = do
   let key               = "XYZ"
-  let contents          = BC8.pack "abracadabra"
-  let encryptedExpected = BC8.pack "9;(9:;<88*8"
+  let contents          = "abracadabra"
+  let encryptedExpected = "9;(9:;<88*8"
   let encrypted         = encrypt key contents
   let decrypted         = decrypt key encrypted
   print   encrypted
   print $ encrypted == encryptedExpected
   print   decrypted
-  print $ decrypted == BC8.unpack contents
+  print $ decrypted == contents
 
 
 
+-- NOTE Program crashes if:
+--      1. Pattern match fails
+--      2. File does not exist (also consider case of mixing arg order)
 main :: IO ()
-main = undefined
--- TODO
--- * It should accept the file name and the key as command-line arguments
--- * Use bytestring to read the file
--- * Encode the key as UTF8 (with encodeUtf8)
--- * Overwrite the file with encryped variant
+main = do
+  args <- Env.getArgs
+  let [keyString, filePath] = args
+  let key = (Enc.encodeUtf8 . Text.pack) keyString
+  contents <- B.readFile filePath
+  putStrLn "==> Reading File"
+  let encrypted = encrypt key contents
+  putStrLn "==> Flipping Encryption"
+  seq (B.length contents) (B.writeFile filePath encrypted)
+  putStrLn "==> Writing File"
+  putStrLn "==> Done"
+  putStrLn "==> Result:\n"
+  BC8.putStrLn encrypted
