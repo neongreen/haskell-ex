ccd3b4c09ba196059692fee308028672c132a370
Jason Kuhrt <jasonkuhrt@me.com>
2016-09-28 14:43:13 -0400

    justify: Support paragraphs with words longer than line.

--- week4/justify/jasonkuhrt/Main.hs
+++ week4/justify/jasonkuhrt/Main.hs
@@ -48,8 +48,13 @@ import Data.List
 sample :: String
 sample = "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness..."
 
+longSample = replicate 66 '-' ++ " " ++ sample
+
 demo :: IO ()
-demo = putStrLn $ (justify 65 . setParagraph 65) sample
+demo = do
+  putStrLn $ (justify 65 . setParagraph 65) sample
+  putStrLn $ (justify 65 . setParagraph 65) longSample
+
 
 
 
@@ -113,17 +118,30 @@ setParagraph w string
   -- Stop once the string is shorter than a single line.
   | w >= length string = string
   | otherwise          =
-    let (nextLine, rest) = splitAt (nextLineWidth w string) string
-     in nextLine ++ "\n" ++ setParagraph w (drop 1 rest) -- Drop the space
+    let (nextLine, string') = splitAtNextLine w string
+     in nextLine ++ "\n" ++ setParagraph w string'
 
 
 
 -- | Find the longest line of whole words that do not exceed limit.
-nextLineWidth :: Int -> String -> Int
-nextLineWidth = closestBefore ' '
--- Solution is to find a limit followed by a space. Since limit is
--- 1-based but closestBefore takes an index, we are searching
--- the trailing character of limit.
+splitAtNextLine :: Int -> String -> (String,String)
+splitAtNextLine w string =
+  go werd (unwords werds)
+  where
+  ([werd], werds) = splitAt 1 . words $ string
+  go :: String -> String -> (String, String)
+  go nextLine string
+    | length nextLine >= w = (nextLine, string)
+    | otherwise            =
+      case (elemIndex ' ' string) of
+      Nothing -> (nextLine, string)
+      Just i  ->
+        if length nextLine + i > w
+        then
+          (nextLine, string)
+        else
+          let (nextWord, (_:string')) = splitAt (i) string
+          in go (nextLine ++ " " ++ nextWord) string'
 
 
 
