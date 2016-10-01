4f5a8074116d0b766d1ef7bc4f6ef4a44e9c4e9e
Thales Macedo Garitezi <thalesmg@gmail.com>
2016-10-01 19:59:18 -0300

    Correcting data type and incorporating suggestions

--- week4/trie/thalesmg/Main.hs
+++ week4/trie/thalesmg/Main.hs
@@ -1,56 +1,57 @@
 import qualified Data.Map.Lazy as M
+import Control.Monad (forever)
+import Data.Map.Lazy (Map)
 
-data Trie a = Empty | Node (M.Map a (Trie a)) deriving (Show)
+data Trie a = Empty | Node Bool (Map a (Trie a)) deriving (Show)
 
-mkTrie :: [a] -> Trie a
-mkTrie [] = Empty
-mkTrie (c:cs) = Node (M.singleton c (mkTrie cs))
+singletonTrie :: [a] -> Bool -> Trie a
+singletonTrie [] _ = Empty
+singletonTrie [c] b = Node b (M.singleton c Empty)
+singletonTrie (c:cs) b = Node b (M.singleton c (singletonTrie cs False))
 
 addToTrie :: (Ord a) => Trie a -> [a] -> Trie a
 addToTrie t [] = t
-addToTrie Empty xs = mkTrie xs
-addToTrie (Node mp) (x:xs) = 
+addToTrie Empty xs = singletonTrie xs False
+addToTrie (Node bool mp) (x:xs) = 
   case M.lookup x mp of
-    Nothing -> Node (M.insert x (mkTrie xs) mp)
-    Just t' -> Node (M.insert x (addToTrie t' xs) mp)
+    Nothing -> Node bool (M.insert x (singletonTrie xs False) mp)
+    Just Empty -> Node bool (M.insert x (singletonTrie xs True) mp)
+    Just t' -> Node bool (M.insert x (addToTrie t' xs) mp)
     
 countNodes :: Trie a -> Int
 countNodes t =
   let
     count' Empty acc = acc
-    count' (Node mp) acc = M.foldr count' (acc + M.size mp) mp
+    count' (Node _ mp) acc = M.foldr count' (acc + M.size mp) mp
   in
     count' t 0
     
 -- Shorter, cleaner version
 countNodes2 :: Trie a -> Int
 countNodes2 Empty = 0
-countNodes2 (Node mp) = M.size mp + sum(M.map countNodes2 mp)
+countNodes2 (Node _ mp) = M.size mp + sum(M.map countNodes2 mp)
     
 queryTrie :: (Ord a) => Trie a -> [a] -> [[a]]
 queryTrie Empty _ = [[]]
-queryTrie (Node mp) [] =
-  do
-    c <- M.keys mp
-    let t = mp M.! c
-    rest <- queryTrie t []
-    return $ c : rest
-queryTrie (Node mp) (x:xs)
-  | x `M.notMember` mp = []
-  | otherwise = 
-      do
-        let t = mp M.! x
-        rest <- queryTrie t xs
-        return (x : rest)
+queryTrie (Node bool mp) [] =
+  if bool then [] : ws else ws
+  where
+  ws = do
+          c <- M.keys mp
+          let t = mp M.! c
+          rest <- queryTrie t []
+          return $ c:rest
+queryTrie (Node _ mp) (x:xs) = case M.lookup x mp of
+  Nothing -> []
+  Just t -> do
+              rest <- queryTrie t xs
+              return (x : rest)
 
 main :: IO ()
 main = do
   ws <- lines <$> readFile "../../../data/words"
   let trie = foldl addToTrie Empty ws
   putStrLn $ "Trie created. There are " ++ show (countNodes2 trie) ++ " nodes."
-  loop trie
-  where
-    loop t = do
-      query <- getLine
-      putStrLn . unwords $ queryTrie t query
-      loop t
+  forever $ do
+    query <- getLine
+    putStrLn . unwords $ queryTrie trie query
