b9723b2b5c0b6a73196f32257f4da72aacc89e59
Jason Kuhrt <jasonkuhrt@me.com>
2016-09-30 21:53:12 -0400

    trie: Tidy

--- week4/trie/jasonkuhrt/Main.hs
+++ week4/trie/jasonkuhrt/Main.hs
@@ -24,7 +24,10 @@ import qualified Data.Map as Map
 
 {- Trie type.
 
-In our model it might at first seem that one word coresponds to one path within a trie of letters, but that would be wrong; Paths can actually naturally represent multiple words since words can also be the prefix of other words. For example:
+In our model it might at first seem that one word coresponds to one path
+within a trie of letters, but that would be wrong; Paths can actually
+naturally represent multiple words since words can also be the prefix of
+other words. For example:
 
      a and
     be bean
@@ -39,7 +42,9 @@ As a visualized trie:
                   |
                   n
 
-So our solution is to model each Node with a flag indicating whether it represents a valid word or not at that point in the path. For example, the above visualization in pseudo code:
+So our solution is to model each Node with a flag indicating whether it
+represents a valid word or not at that point in the path. For example, the
+above visualization in pseudo code:
 
     Node False
       a : Node True
@@ -49,7 +54,9 @@ So our solution is to model each Node with a flag indicating whether it represen
           a : Node False
             n : Empty
 
-Finally each letter (a Node) may branch out to zero, one, or many other letters (nodes) which themselves finish or continue toward another word. For example:
+Finally each letter (a Node) may branch out to zero, one, or many other
+letters (nodes) which themselves finish or continue toward another word.
+For example:
 
         b
        /
@@ -59,21 +66,71 @@ Finally each letter (a Node) may branch out to zero, one, or many other letters 
       |  |
       r  n
 
-Our solution is to use a Map data type which permits us to model arbitrary number of children per node.
+Our solution is to use a Map data type which permits us to model arbitrary
+number of children per node.
 -}
 data Trie a =
   Empty |
   Node Bool (Map a (Trie a))
   deriving (Show)
 
+sample :: Trie Char
+sample =
+  Node False (Map.fromList [
+    ('a', Node True (Map.fromList [
+      ('n', Empty)
+    ])),
+    ('b', Node False (Map.fromList [
+      ('e', Node True (Map.fromList [
+        ('a', Node False (Map.fromList [
+          ('n', Empty)
+        ]))
+      ]))
+    ]))
+  ])
 
 
-{- Index a dictionary of words. -}
-indexWords :: [String] -> Trie Char
-indexWords = foldl indexWord (Node False Map.empty)
+
+-- | Interactive search on the command line.
+main :: IO ()
+main = do
+  putStr "==> Indexing dictionary... "
+  trie <- pure . wordsToTrie . lines =<< readFile "/usr/share/dict/words"
+  putStrLn "Done!"
+  putStrLn "==> Now search for words by prefix."
+  prompt trie
+  where
+  prompt trie = do
+    putStr "> "
+    putStrLn . unwords . flip search trie =<< getLine
+    putStrLn ""
+    prompt trie
+
+
+
+{- Index a dictionary of words.
+
+For example:
+
+  > wordsToTrie ["bet", "be", "bean","beam"]
+
+  Node False (fromList [
+    ('b', Node False (fromList [
+      ('e', Node True (fromList [
+        ('t', Empty),
+        ('a', Node False (fromList [
+          ('m', Empty),
+          ('n',Empty)
+        ]))
+      ]))
+    ]))
+  ])
+-}
+wordsToTrie :: [String] -> Trie Char
+wordsToTrie = foldl indexWord (Node False Map.empty)
 
 indexWord :: Trie Char -> String -> Trie Char
--- Finished indexing word
+-- Finished indexing word whose entire path already existed
 indexWord (Node _ branches)  ""         = Node True branches
 -- Extend a trie path
 indexWord Empty     string              = stringToTrie True string
@@ -91,7 +148,13 @@ stringToTrie isWord (c:cs) = Node isWord (Map.singleton c (stringToTrie False cs
 
 
 
+-- | Find words in given trie that have given prefix.
+search :: String -> Trie Char -> [String]
+search string = trieToWords . findTrieWithPrefix string
+
 {- | Convert a trie into a list of words therein.
+
+TODO Refactor!
 
 For example:
 
@@ -127,12 +190,12 @@ trieToWords (Node _ m) = concat . Map.elems $ Map.mapWithKey (go "") m
     ((prefix ++ [char]) :) . concat . Map.elems $
     Map.mapWithKey (go (prefix ++ [char])) maap
 
-
-
-
 {- | Find the trie matching a given prefix.
 
-The trie returned does include the prefix given but all isWord flags are set to False. For example given a trie with the four words "be", "beam", "bean", "bet" and a prefix of "bea" then the result excludes "t" branch as well as unmarking "be" as a word. Observe:
+The trie returned does include the prefix given but all isWord flags are set
+to False. For example given a trie with the four words "be", "beam", "bean",
+"bet" and a prefix of "bea" then the result excludes "t" branch as well as
+unmarking "be" as a word. Observe:
 
   trie =
     Node False (fromList [
@@ -169,41 +232,3 @@ findTrieWithPrefix prefix trie = fromMaybe Empty (go prefix trie)
     case Map.lookup char branches of
       Nothing -> Nothing
       Just t  -> fmap (Node False . Map.singleton char) (go cs t)
-
-
-
-{- | Find words in given trie that have given prefix. -}
-search :: String -> Trie Char -> [String]
-search string = trieToWords . findTrieWithPrefix string
-
-
-
-{- | Interactive search on the command line. -}
-main :: IO ()
-main = do
-  putStr "==> Indexing dictionary... "
-  trie <- pure . indexWords .  lines =<<  readFile "/usr/share/dict/words"
-  putStrLn "Done!"
-  putStrLn "==> Now search for words by prefix."
-  prompt trie
-  where
-  prompt trie = do
-    putStr "> "
-    putStrLn . unwords . flip search trie =<< getLine
-    putStrLn ""
-    prompt trie
-
-sample :: Trie Char
-sample =
-  Node False (Map.fromList [
-    ('a', Node True (Map.fromList [
-      ('n', Empty)
-    ])),
-    ('b', Node False (Map.fromList [
-      ('e', Node True (Map.fromList [
-        ('a', Node False (Map.fromList [
-          ('n', Empty)
-        ]))
-      ]))
-    ]))
-  ])
