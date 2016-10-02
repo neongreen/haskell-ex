d4b87b421695d2d58f868cad2a94cff8e3b7449f
Thales Macedo Garitezi <thalesmg@gmail.com>
2016-10-02 17:15:25 -0300

    Fixed algorithm and requirements for task.
    Fixed Dijkstra's algorithm (it was visiting nodes in the wrong order);
    Now the start and end points are read automatically and are shown in the result;
    Now the exception is not thrown if there is no path.

--- week4/path/thalesmg/Main.hs
+++ week4/path/thalesmg/Main.hs
@@ -1,5 +1,8 @@
 import qualified Data.Map.Lazy as M
+import qualified Data.Set as S
+import Data.Set (Set)
 import Data.List (sortOn)
+import Debug.Trace (trace)
 
 -- |A Node is simply a pair of coordinates.
 type Node = (Int, Int)
@@ -94,8 +97,11 @@ readGraph2 rows =
   Given a connected, unexplored Graph and a starting Node, 
   attempts to find the shortest path using Dijksta's algorithm.
 -}
-dijkstra :: Graph -> Node -> Path
-dijkstra graph start =
+dijkstra :: Graph   -- ^Starting graph
+            -> Node -- ^Starting node.
+            -> Node -- ^End node.
+            -> Path -- ^Resulting path.
+dijkstra graph start end =
   let
     updateNode :: (Graph, Path) -> Node -> Node -> Double -> (Graph, Path)
     updateNode (g', p) parent target distance = 
@@ -105,35 +111,44 @@ dijkstra graph start =
       where
         oldDistance = fst $ g' M.! target
         delta = sqrt . fromIntegral $ (fst parent - fst target)^2 + (snd parent - snd target)^2
-    go :: [Node] -> (Graph, Path) -> Path
-    go [] (_, path) = path
-    go open (g, path) = 
-      let
-        next:rest = sortOn fst open
-        (currDist, neighs) = g M.! next
-        g'p = foldl (\acc n -> updateNode acc next n currDist) (g, path) neighs
-      in
-        go rest g'p
+    go ::  Set Node -- ^Open set 
+           -> Set Node -- ^Closed set
+           -> (Graph, Path) 
+           -> Path
+    go open closed (g, path)
+      | S.null open = M.empty -- |If there are no nodes in the open set and we
+                              --  still didn't return the path, there is no path.
+      | otherwise =
+        let
+          (next, rest) = S.deleteFindMin open
+          closed' = S.insert next closed
+          (currDist, neighs) = g M.! next
+          (g', p) = foldl (\acc n -> updateNode acc next n currDist) (g, path) neighs
+          open' = foldr S.insert rest neighs `S.difference` closed 
+        in
+          if end `M.member` p then p else go open' closed' (g', p)
     startGraph = updateNode (graph, M.empty) start start 0
-    allNodes = M.keys (fst startGraph)
-  in 
-    go allNodes startGraph
+    --   allNodes = M.keys (fst startGraph)
+    in 
+      go (S.singleton start) S.empty startGraph
     
 {-|
   Given a target end Node and a map of Paths, returns the list of steps needed
   to get from the start Node to the end Node.
 -}
 getPath :: Node -> Path -> [Node]
-getPath end paths =
-  let
-    go :: Node -> [Node] -> [Node]
-    go current acc = if paths M.! current == current 
-                       then current:acc
-                       else go previous (current:acc)
-                     where
-                       previous = paths M.! current
-  in
-    go end []
+getPath end paths
+  | M.null paths = []
+  | otherwise =
+    let
+      go :: Node -> [Node] -> [Node]
+      go current acc = if paths M.! current == current 
+                         then current:acc
+                         else go previous (current:acc)
+                       where
+                         previous = paths M.! current
+    in
+      go end []
       
 {-|
   Given a Path calculated by getPath and the original String map,
@@ -143,17 +158,27 @@ drawPath :: [Node] -> [String] -> [String]
 drawPath path rawMap =
   [[changeChar c (x, y) | (c, y) <- zip row [0..]] | (row, x) <- zip rawMap [0..]]
   where
-    changeChar c node = if node `elem` path then '+' else c
+    changeChar c node = if node `elem` path && c /= 'A' && c /= 'B'
+                            then '+' 
+                            else c
 
 main :: IO ()
 main = do
-  rawMap <- lines <$> readFile "sampleMap.txt"
-  let (start, end, graph) = readGraph rawMap
-      processedGraph = dijkstra graph start
-      path = getPath end processedGraph
-  mapM_ putStrLn $ drawPath path rawMap
+  rawMap <- lines <$> readFile "sample2"
+  mapM_ putStrLn rawMap
   putStrLn ""
+  let (start, end, graph) = readGraph rawMap
+      processedGraph = dijkstra graph start end
+      path = getPath end processedGraph
+  putStrLn "Without diagonals:"
+  if null path
+    then putStrLn "There is no path!!!"
+    else mapM_ putStrLn $ drawPath path rawMap
+  putStrLn ""
+  putStrLn "With diagonals:"
   let (start, end, graph2) = readGraph2 rawMap
-      processedGraph2 = dijkstra graph2 start
+      processedGraph2 = dijkstra graph2 start end
       path2 = getPath end processedGraph2
-  mapM_ putStrLn $ drawPath path2 rawMap
+  if null path2
+    then putStrLn "There is no path!!!"
+    else mapM_ putStrLn $ drawPath path2 rawMap
