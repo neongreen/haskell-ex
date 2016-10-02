a55943671958d770d2f9879be646efef66a17d3d
Thales Macedo Garitezi <thalesmg@gmail.com>
2016-10-02 17:29:06 -0300

    Added escaping, but ripped off Artyom's function =(

--- week4/json-print/thalesmg/Main.hs
+++ week4/json-print/thalesmg/Main.hs
@@ -1,23 +1,38 @@
 import Data.Map (Map)
 import Data.List (intercalate)
 import qualified Data.Map as M
+import Numeric (showHex)
 
 data JSON = JNull | JNum Double | JString String | JArray [JSON] | JObj (Map String JSON) | JBool Bool
+
+escapeUnicode :: Char -> String
+escapeUnicode c = "\\u" ++ replicate (4 - length h) '0' ++ h
+ where
+   h = showHex (fromEnum c) ""
+
+escape :: String -> String
+escape s = "\"" ++ concatMap escapeChar s ++ "\""
+ where
+   escapeChar '"'  = "\\\""
+   escapeChar '\\' = "\\\\"
+   escapeChar '\b' = "\\b"
+   escapeChar '\f' = "\\f"
+   escapeChar '\n' = "\\n"
+   escapeChar '\r' = "\\r"
+   escapeChar '\t' = "\\t"
+   escapeChar x | x < '\x20' = escapeUnicode x
+   escapeChar x = [x]
 
 instance Show JSON where
   show JNull = "null"
   show (JBool b) = if b then "true" else "false"
   show (JNum x) = show x
-  show (JString s) = "\"" ++ escape s ++ "\""
-    where
-      escape =
-        foldr (\c -> (++) (if c == '\"' then "\\\"" else [c])) []
-  -- show (JArray []) = "[]"
+  show (JString s) = escape s
   show (JArray objs) = "[" ++ intercalate "," (map show objs) ++ "]"
   show (JObj m) = "{" ++ intercalate "," (elems m) ++ "}"
     where
       elems x = 
-        map (\(key,value) -> ("\"" ++ key ++ "\":" ++ show value)) (M.toList x)
+        map (\(key,value) -> (escape key ++ ":" ++ show value)) (M.toList x)
 
 main :: IO ()
 main = do
@@ -28,4 +43,6 @@ main = do
   print $ JArray [JNull, JBool False, JArray []]
   print $ JObj M.empty
   print $ JObj $ M.fromList [("field1", JNull), ("field2", JString "A \" quote!"),
-                              ("field3", JObj $ M.singleton "subField" (JArray [])) ]
+                              ("field3", JObj $ M.singleton "subField" (JArray []))]
+  -- putStrLn "A new\nline!"
+  print $ JString "A new\nline!"
