e7937cb092f956c36182f03783af22bc67424459
Balachandran Chandrasekharan <balachandran.c@gmail.com>
2016-10-04 13:35:00 +0530

    quine: rename variables

--- week5/quine/Main.hs
+++ week5/quine/Main.hs
@@ -1,6 +1,6 @@
 import Data.Char
 import Data.List
 main = do
-    let bracket str = putStr ( intercalate [chr 34] str ) >> putStrLn ( show str ) >> putStr "    bracket prefix"
-    let prefix = ["import Data.Char\nimport Data.List\nmain = do\n    let bracket str = putStr ( intercalate [chr 34] str ) >> putStrLn ( show str ) >> putStr ","    bracket prefix","\n    let prefix = "]
-    bracket prefix
+    let bracket arr = putStr ( intercalate [chr 34] arr ) >> putStrLn ( show arr ) >> putStr "    bracket codeArray"
+    let codeArray = ["import Data.Char\nimport Data.List\nmain = do\n    let bracket arr = putStr ( intercalate [chr 34] arr ) >> putStrLn ( show arr ) >> putStr ","    bracket codeArray","\n    let codeArray = "]
+    bracket codeArray
