cd1fb741d505977c4787e9a0b6afa10287f6729f
Vitalie Cracan <vitalie.cracan@gmail.com>
2016-10-06 12:20:16 +0300

    quine: replace custom quote function with built-in show

--- week5/quine/vitcra/Main.hs
+++ week5/quine/vitcra/Main.hs
@@ -2,20 +2,18 @@ module Main where
 
 import Data.Char (chr)
 
-quote s = chr 34 : s ++ [chr 34]
 sp1 = ([chr 32, chr 32, chr 32, chr 32] ++)
 sp2 = ([chr 32, chr 32, chr 44, chr 32] ++)
-h = 10
+h = 9
 
 self = [
     "module Main where"
   , ""
   , "import Data.Char (chr)"
   , ""
-  , "quote s = chr 34 : s ++ [chr 34]"
   , "sp1 = ([chr 32, chr 32, chr 32, chr 32] ++)"
   , "sp2 = ([chr 32, chr 32, chr 44, chr 32] ++)"
-  , "h = 10"
+  , "h = 9"
   , ""
   , "self = ["
   , "  ]"
@@ -23,14 +21,14 @@ self = [
   , "main :: IO ()"
   , "main = do"
   , "  mapM_ putStrLn (take h self)"
-  , "  putStrLn $ sp1 . quote $ head self"
-  , "  mapM_ (putStrLn . sp2 . quote) $ tail self"
+  , "  putStrLn $ sp1 . show $ head self"
+  , "  mapM_ (putStrLn . sp2 . show) $ tail self"
   , "  mapM_ putStrLn (drop h self)"
   ]
 
 main :: IO ()
 main = do
   mapM_ putStrLn (take h self)
-  putStrLn $ sp1 . quote $ head self
-  mapM_ (putStrLn . sp2 . quote) $ tail self
+  putStrLn $ sp1 . show $ head self
+  mapM_ (putStrLn . sp2 . show) $ tail self
   mapM_ putStrLn (drop h self)
