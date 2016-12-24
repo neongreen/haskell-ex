89cd9fd16c49bc0efac9f2f2b90f28bab339b1cf
Boris M. Yartsev <borboss366@users.noreply.github.com>
2016-12-24 18:58:13 +0300

    Solved the scary task from the week 1

---
+++ week1/scary/borboss366/Main.hs
@@ -0,0 +1,17 @@
+module Main where
+import System.IO (openFile)
+import Data.Char (toLower, ord, isAsciiLower)
+
+main :: IO ()
+main = processFile "/usr/share/dict/words"
+
+processFile inputFile = do
+  input <- readFile inputFile
+  let count = length $ filter isScary $ lines input
+  putStrLn $ "Total scary words " ++ show count
+
+isScary :: String -> Bool
+isScary word = countSum word == 13
+
+countSum :: String -> Int
+countSum = sum . map (\c -> ord c - ord 'a') . filter isAsciiLower . map toLower
